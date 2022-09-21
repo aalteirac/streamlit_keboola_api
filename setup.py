002c2e1fc2b162169167fadfa5589d67291b4a75
aalteirac <anthony@alteirac.com>
2022-09-21 12:49:04 +0200

    cleaning import

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup( 
     name="streamlit-keboola-api",
-    version="0.1.0",
+    version="0.1.7",
     author="Anthony Alteirac",
     author_email="anthony.alteirac@snowflake.com",
     description="Wrapper around Kebolla python client to list tables, buckets and upload files to stage",
