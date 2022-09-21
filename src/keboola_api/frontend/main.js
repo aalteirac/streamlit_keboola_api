3f534979cefb8f4d5fa31c3fc2315eeb2249ca72
aalteirac <anthony@alteirac.com>
2022-09-21 23:42:20 +0200

    api mode is now really invisible

--- src/keboola_api/frontend/main.js
+++ src/keboola_api/frontend/main.js
@@ -3,12 +3,24 @@ function sendValue(value) {
   Streamlit.setComponentValue(value)
 }
 
+function getParentElement()   {
+  var ifs = window.top.document.getElementsByTagName("iframe");
+  for(var i = 0, len = ifs.length; i < len; i++)  {
+     var f = ifs[i];
+     var fDoc = f.contentDocument || f.contentWindow.document;
+     if(fDoc === document)   {
+        return f.parentElement
+     }
+  }
+}
+
 function onRender(event) {
     const {label,key,api_only} = event.detail.args;
     if(api_only==true){
       if (!window.rendered) {
         sendValue(Math.random()*1000);
         Streamlit.setFrameHeight(0);
+        getParentElement().style.height="0px";
       }
     }
     else{
