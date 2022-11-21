9a7a7820a410ab5d572656b6cb2a1b4b0b33d461
Anthony Alteirac <anthony@alteirac.com>
2022-11-21 13:18:31 +0100

    bump for pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup( 
     name="streamlit-keboola-api",
-    version="0.1.9",
+    version="0.2.0",
     author="Anthony Alteirac",
     author_email="anthony.alteirac@snowflake.com",
     description="Wrapper around Kebolla python client to list tables, buckets and upload files to stage",
